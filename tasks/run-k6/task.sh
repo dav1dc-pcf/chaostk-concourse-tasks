f4207ce77bd7ff343613317c77a465fa2c9c6e5b
David Crawford <dcrawford@pivotal.io>
2021-11-21 11:38:38 -0500

    Initial commit of a simple pipeline to kick off k6 tests

---
+++ tasks/run-k6/task.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# DEBUG
+set -x
+
+# Generate a k6 config file for a simple test
+echo -ne "import http from 'k6/http';\n\nexport default function () {\n  http.get(\"${SITE_TO_TEST}\");\n}\n" > http_get.js
+
+# DO IT!
+k6 -u ${VIRUTAL_USERS} -i ${RQUEST_COUNT} -d ${TEST_DURATION_LIMIT} http_get.js
