484c18880e4d993edf443301e6368d593676ac56
David Crawford <dcrawford@pivotal.io>
2021-11-23 15:38:09 -0500

    Add messages to chaostk-turb-rand custom task that prints the selected values for JOB_NAME and LIMIT values selected randomly

--- tasks/run-chaostk-turb-rand/task.sh
+++ tasks/run-chaostk-turb-rand/task.sh
@@ -14,6 +14,14 @@ job_arr=( $(echo ${RANDOM_JOB_NAME_ARR} | jq -r .[]) )
 export JOB_NAME=${job_arr[$RANDOM % ${#job_arr[@]} ]}
 export LIMIT=$(( ( $RANDOM % ${RANDOM_LIMIT_RANGE} )  + 1 ))
 
+# Let the good people know what was randomly selected.
+echo "Selected the following random values:"
+echo "JOB_NAME=" ${JOB_NAME}
+echo "LIMIT=" ${LIMIT}
+echo
+echo "Sleeping for 10 seconds in case you want to abort this selection..."
+sleep 5
+
 # DO IT!
 chaos run ${CHAOSTK_CMD_FLAGS} ${EXPERIMENT_FILE}
 
