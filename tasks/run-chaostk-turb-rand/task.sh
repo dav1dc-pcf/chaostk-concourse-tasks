37e9a9f17ce956118b244b42e676f5caaec394bb
David Crawford <dcrawford@pivotal.io>
2021-12-02 11:09:48 -0500

    Copy Pasta the new cURL to the Turbulence API Server end-point for the incident details over to the Turbulence Random task.sh file

--- tasks/run-chaostk-turb-rand/task.sh
+++ tasks/run-chaostk-turb-rand/task.sh
@@ -30,4 +30,19 @@ echo
 echo
 cat chaostoolkit.log
 
+# Could we extract a Turbulence Attack ID from the chaostoolkit.log file??
+CHECK=$(grep "attack submitted sucessfully" chaostoolkit.log | wc -l)
+
+if [[ "${CHECK}" -eq "1" ]]; then
+  ID=$(grep '"ID"' chaostoolkit.log | grep "-" | uniq | awk '{print $2}' | tr -d ',' | sed 's/"//g')
+
+  echo "Sleeping for 60 seconds before fetching attack log from Turbulence API Server..."
+  sleep 60
+
+  if [[ ! -z "${ID}" ]]; then
+    URL_TO_CURL="${TURBULENCE_URL}/incidents/${ID}"
+    curl -k ${URL_TO_CURL}
+  fi
+fi
+
 # FIN
